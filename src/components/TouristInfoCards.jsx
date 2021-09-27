ec5c6083e124b08e464660aa07c2c40ed2581173
wandraded <wandraded4@gmail.com>
2021-09-27 22:28:25 +0200

    Tarea-Hotel-REACT

--- src/components/TouristInfoCards.jsx
+++ src/components/TouristInfoCards.jsx
@@ -5,21 +5,21 @@ const TouristInfoCards = () => {
     {
       city: "Glasgow",
       image:
-        "https://th.bing.com/th/id/OIP.i4RVpAe-j7X3ntfKc20k2AHaEr?pid=ImgDet&rs=1",
+        "https://www.hisour.com/wp-content/uploads/2018/05/Architecture-in-Glasgow.jpg",
       description: "Glasgow City",
       url: "https://peoplemakeglasgow.com/"
     },
     {
       city: "Manchester",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/a/a8/Manchester_Town_Hall_by_night_from_Renaissance_Hotel.JPG",
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/06/3d/31/60/manchester-town-hall.jpg?w=700&h=-1&s=1",
       description: "Manchester City",
       url: "https://www.visitmanchester.com/"
     },
     {
       city: "London",
       image:
-        "https://th.bing.com/th/id/R.3b835f25c035327c5d459414837a924b?rik=TMgyGT6Coc545Q&pid=ImgRaw&r=0",
+        "https://cdn.londonandpartners.com/-/media/images/london/visit/things-to-do/sightseeing/london-attractions/tower-bridge/thames_copyright_visitlondon_antoinebuchet640x360.jpg?mw=640&hash=27AEBE2D1B7279A196CC1B4548638A9679BE107A",
       description: "London City",
       url: "https://visitlondon.com/"
     }
@@ -30,7 +30,7 @@ const TouristInfoCards = () => {
       {cities.map(({ city, image, description, url }, index) => {
         return (
           <div key={index} className="card m-3">
-            <img src={image} className="card-img-top" alt="..." />
+            <img src={image} className="card-img-top img-thumbnail" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{city}</h5>
               <p className="card-text">{description}</p>
